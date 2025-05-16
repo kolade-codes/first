b30fb6bbc4bfc83ce6bf4bf6b75e3e51ee6930d6
kolade-codes <teniolakolawole899@gmail.com>
2025-05-16 03:44:33 -0700

    Initial commit - added HTML and JS files

---
+++ index.js
@@ -0,0 +1,19 @@
+// const incrementBtn = document.getElementById('increment-btn')
+// const countDisplay = document.getElementById('count-display');
+
+// let currentCount = 0
+
+// incrementBtn.addEventListener('click', () =>{
+//     currentCount++
+//     countDisplay.innerText = currentCount
+// })
+
+// const incrementBtn2 = document.getElementById('increment-btn2')
+// const countDisplay2 = document.getElementById('count-display2');
+
+// let currentCount2 = 0
+
+// incrementBtn2.addEventListener('click', ()=>{
+//     currentCount2++
+//     countDisplay2.innerText = currentCount2
+// })
